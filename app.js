a90b1756a080d50b2edc13027a2417c3b07c3f43
RobinGdt <gdt.robin@gmail.com>
2022-03-15 16:32:23 +0100

    ajout copy drop dans draw

--- app.js
+++ app.js
@@ -1,33 +1,41 @@
 const rectangle = document.querySelector('.formeRectangle');
 const draw = document.querySelector('.container1');
-
-// AJOUT DES EVENEMENTS DRAG 
-
-rectangle.addEventListener('dragstart', dragStart);
-rectangle.addEventListener('dragend', dragEnd);
-
-// AJOUT DES EVENEMENTS DRAGOVER
+const box = document.querySelectorAll('.box');
+const rect = document.querySelector('.rectangle')
+let dragItem = null;
 
 draw.addEventListener('dragover', dragOver);
 draw.addEventListener('dragenter', dragEnter);
 draw.addEventListener('dragleave', dragLeave);
 draw.addEventListener('drop', dragDrop);
 
+for(let i of box){
+    i.addEventListener('dragstart', dragStart);
+    i.addEventListener('dragend', dragEnd);
 
-// FONCTIONS DES EVENEMENTS DRAG
+    const clone = i.cloneNode(true);
 
+    i.addEventListener ('click',() => {
+        draw.append(i);
+        i.style.top = 410 + "px";
+        i.style.left = 410 + "px";
+        console.log('collé');
+
+    });
+};
 
 function dragStart() {
+    dragItem = this;
     this.className += ' tenu';
-}
+};
 
 function dragEnd() {
-
-}
+    dragItem = this;
+    rect.append(dragItem)
+};
 
 function dragOver(e) {
     e.preventDefault();
-
 };
 
 function dragEnter(e) {
@@ -41,10 +49,33 @@ function dragLeave() {
 
 function dragDrop(e) {
     this.className = 'container1';
-    this.append(rectangle);
-    let x = e.pageX;
-    let y = e.pageY;
 
-    rectangle.style.top = y + "px";
-    rectangle.style.left = x + "px";
+    this.append(dragItem);
+    let ex = e.pageX;
+    let ey = e.pageY;
+
+    dragItem.style.top = ey + "px";
+    dragItem.style.left = ex + "px";
+
 };
+
+let x, y, h, w;
+
+function ts(e) {
+    x = e.touches[0].clientX;
+    y = e.touches[0].clientY;
+
+    h = rectangle.clientHeight;
+    m = rectangle.clientWidth;
+}
+
+function ts(e) {
+    mx = e.touches[0].clientX;
+    my = e.touches[0].clientY;
+
+    cx = mx - x;
+    cy = my - y;
+
+    rectangle.style.width = cx + w;
+    rectangle.style.width = cy + h;
+}
